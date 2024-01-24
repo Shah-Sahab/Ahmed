9955a0558dcf1dcf9e30c043fd91eba00cd70c13
syedahmedhussain <ShahSahabTheDeveloper@gmail.com>
2024-01-24 00:43:59 -0600

    First version

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+// Sha-bang line (above) to tell the shell how to execute the file
+console.log(`Syed Ahmed Hussain 
+
+Just a life long learner, who loves to code and solve problems. 
+Worked on several different applications, ranging from web to mobile.
+A day dreamer, who wants to make this world a better place to live in. 
+A landscape photographer, who loves to capture the beauty of nature.
+Overall, a simple human being, who wants to live a happy, yet meaningful life.
+
+Twitter: https://twitter.com/Ahmedhussain82
+Github: https://github.com/Shah-Sahab
+Landscapes: https://www.aj-inc.us
+
+`);
