ac6846d7a37b47d46f4e59a9193b20fe9794a759
syedahmedhussain <ShahSahabTheDeveloper@gmail.com>
2024-10-24 21:48:59 -0500

    Updated my Introduction in index.js

--- index.js
+++ index.js
@@ -17,11 +17,15 @@ welcome({
 
 console.log(` 
  🇦 🇭 🇲 🇪 🇩 
-                                   
-Just a life long learner, with a love-hate relationship with coding and solving problems :D. 
-Worked on several different applications, ranging from web to mobile.
+
+Hey there! I'm Ahmed, a software engineer based in the United States. 
+Been in the Software development industry for more than a decade.
+I am a true technologiest at heart. List of technologies I have worked on, Top (current), end(oldest): 
+ReactJS, Typescript, NodeJS, NestJS, ExpressJS, Postgresql, AWS, Docker, Jenkins, Git, Android application development, ASP.NET C#, and many more.
+
+My portfolio is well versed ranging from web to mobile.
 
 Github: https://github.com/Shah-Sahab
-Landscapes: https://www.aj-inc.us
+Landscapes Photography: https://www.aj-inc.us
 
 `);
